f22ec30f73832569cb60f8ca4b46e76800c103e2
Phillip Puckett <phillippuckett88@gmail.com>
2016-03-16 00:05:55 -0600

    Routes Work

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,19 @@
 angular.module('ePortfolio')
     .controller('portfolioCtrl', function ($scope, $state) {
-        $scope.portfolioCtrl = 'PORTFOLIO CONTROLLER';
+        $scope.portfolioCtrl = 'PORTFOLIO';
     });
 
 angular.module('ePortfolio')
     .controller('aboutmeCtrl', function ($scope, $state) {
-        $scope.aboutmeCtrl = 'ABOUTME CONTROLLER';
+        $scope.aboutmeCtrl = 'ABOUTME';
     });
 
 angular.module('ePortfolio')
     .controller('contactmeCtrl', function ($scope, $state) {
-        $scope.contactmeCtrl = 'CONTACTME CONTROLLER';
+        $scope.contactmeCtrl = 'CONTACTME';
     });
 
 angular.module('ePortfolio')
     .controller('resumeCtrl', function ($scope, $state) {
-        $scope.resumeCtrl = 'RESUME CONTROLLER';
+        $scope.resumeCtrl = 'RESUME';
     });
